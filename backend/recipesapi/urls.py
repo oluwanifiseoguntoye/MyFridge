e5c26948e56a25910de30a07a95c7aa8657aba26
Oluwanifise Oguntoye <117686447+oluwanifiseoguntoye@users.noreply.github.com>
2023-03-10 13:08:01 -0500

    chatbot set up (used to pray for times like these)

--- backend/recipesapi/urls.py
+++ backend/recipesapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RecipeFilter, RecipeList
+from .views import RecipeFilter, RecipeList, RecipeBot
 
 urlpatterns = [
     path('recipes/', RecipeList.as_view(), name='recipe-list'),
     path('recipes/filter/', RecipeFilter.as_view(), name='recipe-filter'),
+    path('recipebot/', RecipeBot.as_view(), name='chatbot'),
 ]
--- backend/recipesapi/views.py
+++ backend/recipesapi/views.py
@@ -1,7 +1,20 @@
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Recipe, UserIngredient, RecipeIngredient
 from .serializers import RecipeSerializer
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import nltk 
+import string
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
 
 class RecipeFilter(APIView):
     def post(self, request):
@@ -21,5 +34,56 @@ class RecipeList(APIView):
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
+    
+
+class RecipeBot(APIView):
+
+    def post(self, request):
+        message = request.data['message']
+        
+        # Initialize a chatbot instance
+        bot = ChatBot('Bot')
+        
+        # Train the chatbot with recipes and their ingredients
+        recipes = Recipe.objects.all()
+        trainer = ListTrainer(bot)
+        for recipe in recipes:
+            trainer.train([recipe.recipe] + [ri.ingredient.ingredient for ri in recipe.ingredients.all()])
+        
+        # Extract the ingredients from the user message
+        def extract_ingredients(message):
+            ingredient_list = [ingredient.ingredient.lower() for ingredient in UserIngredient.objects.all()]
+            words = nltk.word_tokenize(message.lower())
+            ingredients = []
+            for word in words:
+                if word in ingredient_list:
+                    ingredients.append(word)
+            print("Input message:", message)
+            print("Tokenized words:", words)
+            print("Ingredients found:", ingredients)
+            return ingredients
 
 
+        
+        ingredients = extract_ingredients(message)
+        
+       # Filter recipes by ingredients
+        def filter_recipes_by_ingredients(ingredients):
+            recipes = Recipe.objects.all()
+            filtered_recipes = []
+            for recipe in recipes:
+                recipe_ingredients = [ri.ingredient.ingredient.lower() for ri in recipe.ingredients.all()]
+                if any(ingredient.lower() in recipe_ingredients for ingredient in ingredients):
+                    filtered_recipes.append(recipe)
+            print(filtered_recipes)
+            return filtered_recipes
+
+        filtered_recipes = filter_recipes_by_ingredients(ingredients)
+
+        # Return recipe description if recipe found
+        if filtered_recipes:
+            recipe = filtered_recipes[0]
+            serializer = RecipeSerializer(recipe)
+            return Response({'message': serializer.data['description']})
+        else:
+            return Response({'message': "I'm sorry, there are currently no recipes available with those ingredients."})
